63004a6d6259e1c4f65caba871dd88fca5d703c2
ujjwalpaul005 <72302529+ujjwalpaul005@users.noreply.github.com>
2020-10-14 00:56:40 +0530

    Update DoThis.py
    Done the challange.

--- DoThis.py
+++ DoThis.py
@@ -3,3 +3,21 @@
 # pi to the power of the first number 
 # or the square root of the second number.
 # then commit and push it back up to github.
+
+import math
+print("My name is Ujjwal.")
+
+a, b = input("Enter two Number separated by space ").split()
+
+print("The larger number is ", end="")
+if a>b:
+    print(a)
+else:
+    print(b)
+
+pi = math.pi
+power = pow(int(a), pi)
+root = math.sqrt(int(b))
+
+print("The first number to the power pi = ", power)
+print("The square root of the second number = ", root)
